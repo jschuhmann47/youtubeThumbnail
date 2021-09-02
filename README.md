e22a874ca3e1559c000025b11ba51d75716b568a
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2021-09-02 14:13:25 -0300

    better validation

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Gets full resolution image from a Youtube video.
 Paste the Youtube URL (it has to be https, not http) and it will open your web browser with the image
+GIFs not supported
--- youtubeThumbnail.py
+++ youtubeThumbnail.py
@@ -1,16 +1,17 @@
 import webbrowser
 
-url=input("Enter URL: ")
-flag=False
-while(not flag):
+url=input("Enter URL: (0 to exit) ")
+while(True):
+    if url=='0':
+        break
     if "https://www.youtube.com/watch?v=" in url:
         id=url[32:43]
-        flag=True
-    elif "http://www.youtube.com/watch?v=" in url:
+        break
+    if "http://www.youtube.com/watch?v=" in url:
         id=url[31:42]
-        flag=True
-    else:
-        url=input("Not a valid URL, enter a new one: ")
-print("ID: " + id)
-urlnueva="http://i3.ytimg.com/vi/"+id+"/maxresdefault.jpg"
-webbrowser.open(urlnueva)
+        break
+    url=input("Invalid URL, enter a new one: (0 to exit) ")
+if url!='0':
+    print("ID: " + id)
+    img="http://i3.ytimg.com/vi/"+id+"/maxresdefault.jpg"
+    webbrowser.open(img)

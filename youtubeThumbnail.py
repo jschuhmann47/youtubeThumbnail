7f927f784af4fdbc44b5091bde68ba022a210c75
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2021-09-02 10:56:16 -0300

    validation

--- youtubeThumbnail.py
+++ youtubeThumbnail.py
@@ -1,7 +1,16 @@
 import webbrowser
 
-url=input("Ingresar URL: ")
-id=url[32:43]
+url=input("Enter URL: ")
+flag=False
+while(not flag):
+    if "https://www.youtube.com/watch?v=" in url:
+        id=url[32:43]
+        flag=True
+    elif "http://www.youtube.com/watch?v=" in url:
+        id=url[31:42]
+        flag=True
+    else:
+        url=input("Not a valid URL, enter a new one: ")
 print("ID: " + id)
 urlnueva="http://i3.ytimg.com/vi/"+id+"/maxresdefault.jpg"
 webbrowser.open(urlnueva)
